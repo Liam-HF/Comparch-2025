fc0816a897f2b67b523dd08865ff589e4d8e9a99
Liam Fries <liamspersonalemail@gmail.com>
2025-09-21 17:36:09 -0400

    Mini-project 1

--- Rainbow Lights/RainbowLights.sv
+++ Rainbow Lights/RainbowLights.sv
@@ -5,14 +5,21 @@ module top(
     output logic RGB_B,
     output logic RGB_G
 );
-
-    parameter LIGHT_INTERVAL = 2000000;
-    logic [$clog2(LIGHT_INTERVAL) - 1:0] count = 0;
-
+    shift_register qvals(
+        .clk(clk),
+        .d(d),
+        .q_1(q_1),
+        .q_2(q_2),
+        .q_3(q_3),
+        .q_4(q_4),
+        .q_5(q_5),
+        .q_6(q_6)
+    );
 
     always_ff @(posedge clk) begin
-        assign RGB_G <= q_5;
-        assign RGB_B <= q_3;
-        assign RGB_R <= q_1;
+        //$display (q_1, q_3, q_5)
+        RGB_G <= q_1;
+        RGB_B <= q_3;
+        RGB_R <= q_5;
     end
 endmodule
--- Rainbow Lights/ShiftRegister.sv
+++ Rainbow Lights/ShiftRegister.sv
@@ -1,7 +1,6 @@
 module shift_register(
     input logic clk,
-    input logic d,
-    input logic reset,
+    output logic d,
     output logic q_1,
     output logic q_2,
     output logic q_3,
@@ -11,28 +10,31 @@ module shift_register(
 );
 
 
-initial begin
-    q_1 <= 1'b1;
-    q_2 <= 1'b1;
-    q_3 <= 1'b1;
-    q_4 <= 1'b0;
-    q_5 <= 1'b0;
-    q_6 <= 1'b0;
-end
+    initial begin
+        q_1 <= 1'b1;
+        q_2 <= 1'b1;
+        q_3 <= 1'b1;
+        q_4 <= 1'b0;
+        q_5 <= 1'b0;
+        q_6 <= 1'b0;
+    end
 
-always_ff @(posedge clk) begin
-    if (count == LIGHT_INTERVAL - 1) begin
-        count <= 0;
-        parameter temp_q <= q_6;
-        assign q_6 <= q_5;
-        assign q_5 <= q_4;
-        assign q_4 <= q_3;
-        assign q_3 <= q_2;
-        assign q_2 <= q_1;
-        assign q_1 <= temp_q;
+    parameter LIGHT_INTERVAL = 2000000;
+    logic [$clog2(LIGHT_INTERVAL) - 1:0] count = 0;
+
+    always_ff @(posedge clk) begin
+        if (count == LIGHT_INTERVAL - 1) begin
+            count <= 0;
+            d <= q_6;
+            q_6 <= q_5;
+            q_5 <= q_4;
+            q_4 <= q_3;
+            q_3 <= q_2;
+            q_2 <= q_1;
+            q_1 <= d;
+        end
+        else begin
+            count <= count + 1;
+        end
     end
-    else begin
-        count <= count + 1;
-    end
-end
 endmodule
